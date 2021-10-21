47d6e068e2b8a5d906d3bc5eecd14f0fd39a54ef
rootkit7628 <arleme.dev7@gmail.com>
2021-10-21 19:25:31 +0300

    Update translation

--- lib/translate.dart
+++ lib/translate.dart
@@ -1,123 +1,129 @@
 var langages =  {
-  'bienvenue': {'fr': 'Bienvenue', 'en': 'Welcome', 'mg': 'Tongasoa'},
-  'incorrect_pass': {
-    'fr': 'Mot de passe ou Nom d\'utilisateur incorrecte',
-    'en': 'Password or Username invalid',
-    'mg': 'Misy diso ny anarana na soratra miafina nampidirinao',
+  "bienvenue": {"fr": "Bienvenue", "en": "Welcome", "mg": "Tongasoa"},
+  "incorrect_pass": {
+    "fr": "Mot de passe ou Nom d\"utilisateur incorrecte",
+    "en": "Password or Username invalid",
+    "mg": "Misy diso ny anarana na soratra miafina nampidirinao",
   },
-  'email_ou_numero_telephone': {
-    'fr': 'Email ou Numero de téléphone',
-    'en': 'Mail or Phone Number',
-    'mg': 'Mailaka na Laharan-tariby',
+  "email_ou_numero_telephone": {
+    "fr": "Email ou Numero de téléphone",
+    "en": "Mail or Phone Number",
+    "mg": "Mailaka na Laharan-tariby",
   },
-  'se_connecter': {
-    'fr': 'connecter',
-    'en': 'connect',
-    'mg': 'Hiditra',
+  "se_connecter": {
+    "fr": "connecter",
+    "en": "connect",
+    "mg": "Hiditra",
   },
-  'erreur_produite': {
-    'fr': 'Une erreur s\'est produite.',
-    'en': 'an error occured',
-    'mg': 'Mandalo olana ara-teknika',
+  "erreur_produite": {
+    "fr": "Une erreur s\"est produite.",
+    "en": "an error occured",
+    "mg": "Mandalo olana ara-teknika",
   },
-  'erreur': {
-    'fr': 'Erreur',
-    'en': 'Error',
-    'mg': 'Hadisoana',
+  "erreur": {
+    "fr": "Erreur",
+    "en": "Error",
+    "mg": "Lesoka",
   },
-  'deconnexion': {
-    'fr': 'Déconnexion',
-    'en': 'Logout',
-    'mg': 'Hivoaka',
+  "deconnexion": {
+    "fr": "Déconnexion",
+    "en": "Logout",
+    "mg": "Hivoaka",
   },
-  'carte': {
-    'fr': 'Carte',
-    'en': 'Maps',
-    'mg': 'Sarin-tany',
+  "carte": {
+    "fr": "Carte",
+    "en": "Maps",
+    "mg": "Sarin-tany",
   },
-  'Ajout_d_un_document': {
-    'fr': 'Ajouter un document',
-    'en': 'Add a document',
-    'mg': 'Fampidirana tahirin-kevitra',
+  "Ajout_d_un_document": {
+    "fr": "Ajouter un document",
+    "en": "Add a document",
+    "mg": "Fampidirana tahirin-kevitra",
   },
-  'Description': {
-    'fr': 'Déscription',
-    'en': 'About',
-    'mg': 'Mombamomba',
+  "Description": {
+    "fr": "Déscription",
+    "en": "About",
+    "mg": "Mombamomba",
   },
-  'Texte': {
-    'fr': 'Texte',
-    'en': 'Text',
-    'mg': 'Lahatsoratra',
+  "Texte": {
+    "fr": "Texte",
+    "en": "Text",
+    "mg": "Lahatsoratra",
   },
-  'Titre': {
-    'fr': 'Titre',
-    'en': 'Title',
-    'mg': 'Lohateny',
+  "Titre": {
+    "fr": "Titre",
+    "en": "Title",
+    "mg": "Lohateny",
   },
-  'Categorie': {
-    'fr': 'Catégorie',
-    'en': 'Category',
-    'mg': 'Sokajy',
+  "Categorie": {
+    "fr": "Catégorie",
+    "en": "Category",
+    "mg": "Sokajy",
   },
-  'Biby': {
-    'fr': 'Animaux',
-    'en': 'Animals',
-    'mg': 'Biby',
+  "Biby": {
+    "fr": "Animaux",
+    "en": "Animals",
+    "mg": "Biby",
   },
-  'Fichier': {
-    'fr': 'Fichier',
-    'en': 'File',
-    'mg': 'Rakitra',
+  "Fichier": {
+    "fr": "Fichier",
+    "en": "File",
+    "mg": "Rakitra",
   },
-  'Recherche': {
-    'fr': 'Recherche',
-    'en': 'Search',
-    'mg': 'Tadiavina',
+  "Recherche": {
+    "fr": "Recherche",
+    "en": "Search",
+    "mg": "Tadiavina",
   },
-  'AJOUTER': {
-    'fr': 'Ajouter',
-    'en': 'Add',
-    'mg': 'Ampidirina',
+  "AJOUTER": {
+    "fr": "Ajouter",
+    "en": "Add",
+    "mg": "Ampidirina",
   },
-  'mot_de_passe': {
-    'fr': 'Mot de passe',
-    'en': 'Password',
-    'mg': 'Teny miafina',
+  "mot_de_passe": {
+    "fr": "Mot de passe",
+    "en": "Password",
+    "mg": "Teny miafina",
   },
-  'mot_de_passe_oublie_?': {
-    'fr': 'Mot de passe oublié ?',
-    'en': 'Forgot your password ?',
-    'mg': 'Hadinonao ve ny teny miafinao ?',
+  "mot_de_passe_oublie_?": {
+    "fr": "Mot de passe oublié ?",
+    "en": "Forgot your password ?",
+    "mg": "Hadinonao ve ny teny miafinao ?",
   },
-  'camera': {
-    'fr': 'camera',
-    'en': 'camara',
-    'mg': 'fakantsary',
+  "camera": {
+    "fr": "camera",
+    "en": "camara",
+    "mg": "fakantsary",
   }, 
-  'DEMANDER_ET_RECEVER' : {
-    'fr': "Demandez et recevez votre permis de construction via Buildeo",
-    'mg' : 'ANTANIO'
+  "DEMANDER_ET_RECEVER" : {
+    "fr": "Demandez et recevez votre permis de construction via Buildeo",
+    "en": "Request and receive your building license by Buildeo",
+    "mg" : "Angataho ary raiso eto amin'ny Buildeo ny fahazoan-dàlana hanorina",
   },
-  'DEMANDER' : {
-    'fr': "Demander",
-    'mg' : 'ANTANIO'
+  "DEMANDER" : {
+    "fr": "Demander",
+    "en": "Request",
+    "mg" : "Angataho",
   },
-  'VERIFICATON_PERMIS' : {
-    'fr': "Vérification de Permis",
-    'mg' : 'ANTANIO'
+  "VERIFICATON_PERMIS" : {
+    "fr": "Vérification de Permis",
+    "en": "License Verification",
+    "mg" : "Fanamarinana fahazoan-dàlana",
   },
-  'SCANNER' : {
-    'fr': "Scanner",
-    'mg' : 'ANTANIO'
+  "SCANNER" : {
+    "fr": "Scanner",
+    "en": "Scanner",
+    "mg" : "Skanera",
   },
-  'NUMERO_DE_PERMIS' : {
-    'fr': "Numéro de permis",
-    'mg' : 'ANTANIO'
+  "NUMERO_DE_PERMIS" : {
+    "fr": "Numéro de permis",
+    "en": "License number",
+    "mg" : "Laharana fahazoan-dàlana",
   },
-   'OU' : {
-    'fr': "OU",
-    'mg' : 'NA'
+   "OU" : {
+    "fr": "OU",
+    "en": "OR",
+    "mg" : "NA",
   },
 
 };
